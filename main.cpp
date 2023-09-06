39238247140b0706a6dbc1d08078f04d7fa399df
unknown <dracospy369@gmail.com>
2023-09-06 16:31:37 -0400

    board_update

--- main.cpp
+++ main.cpp
@@ -1,22 +1,47 @@
-//COMPILE FUNCTION: gcc -o main -Wall main.c -mwindows glut32.lib -lopengl32 -lglu32
+//COMPILE FUNCTION: gcc -o main -Wall main.cpp -mwindows glut32.lib -lopengl32 -lglu32
 
 #include <stdio.h>
+//#include <iostream>
 #include <windows.h>
 #include "GL/glut.h"
-#include <math.h>gcc
+#include <math.h>
+#include <stdlib.h>
+#define xpix 500
+#include <cstring>
+using namespace std;
 #define pi 3.142857
+
+// Function that exits from program
+void keyboard(unsigned char key,
+              int x, int y)
+{
+    switch (key) {
+    case 27:
+        glutHideWindow();
+    }
+}
 
 void display() 
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_POLYGON);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5,  0.5);
-    glVertex2f(0.5,  0.5);
-    glVertex2f(0.5, -0.5);
-    glEnd();
+    for (int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            if ((x + y) % 2 == 1) {
+                glColor3f(1.0, 1.0, 1.0);
+            } else {
+                glColor3f(0, 0, 0);
+            }
 
+            glBegin(GL_POLYGON);
+            glVertex2f(-(x/10), -(y/10));
+            glVertex2f(-(x/10), (y/10));
+            glVertex2f((x/10), (y/10));
+            glVertex2f((x/10), -(y/10));
+            glEnd();
+        }
+    }
+    
     glFlush();
 }
 
@@ -37,7 +62,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
     glutInitWindowPosition(0, 0);
-    glutCreateWindow("Test");
+    glutCreateWindow("Chess");
     glutDisplayFunc(display);
     init();
     glutMainLoop();
